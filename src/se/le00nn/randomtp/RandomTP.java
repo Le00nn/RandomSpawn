f33d39e7daf8295d78caa0388758a876f3df9e03
Le00nn <111663630+Le00nn@users.noreply.github.com>
2023-08-19 09:56:02 +0200

    Initial commit of files

---
+++ src/se/le00nn/randomtp/RandomTP.java
@@ -0,0 +1,107 @@
+// Author: Le00nn
+// Date: 19/08/2023
+// Version: 1.0.0
+
+package se.le00nn.randomtp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RandomTP extends JavaPlugin implements Listener {
+
+	@Override
+	public void onDisable() {
+		Bukkit.getLogger().warning("["+getName(this)+"] (Version: "+getVersion(this)+") disabled!"); // Plugin disabled!
+	}
+
+	@Override
+	public void onEnable() {
+		Bukkit.getLogger().info("["+getName(this)+"] (Version: "+getVersion(this)+") enabled!"); // Plugin enabled!
+		
+		getServer().getPluginManager().registerEvents(this, this);
+		Bukkit.getLogger().info("["+getName(this)+"] Registered events!");
+		
+		getConfiguration().load();
+		
+		getConfiguration().setProperty("config.version", getConfigVersion());
+		getConfiguration().save();
+		Bukkit.getLogger().info("["+getName(this)+"] Using config version " + getConfigVersion() + "!");
+		
+		if(getConfiguration().getProperty("location") == null) {
+			getConfiguration().setProperty("location.a.x", -500);
+			getConfiguration().setProperty("location.a.z", -500);
+			getConfiguration().setProperty("location.b.x", 500);
+			getConfiguration().setProperty("location.b.z", 500);
+			getConfiguration().save();
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		String plname = player.getName();
+		
+		Location loc = calculatePosition(player, plname);
+		if(loc != null) player.teleport(loc);
+		
+		return;
+	}
+	
+	// Calculate position to teleport to on first join.
+	private Location calculatePosition(Player player, String plname) {
+		if(getConfiguration().getProperty("location.hastp."+plname) != null) return null; // Prevent teleport of previously spawned players.
+		
+		int ax = (int)getConfiguration().getProperty("location.a.x");
+		int az = (int)getConfiguration().getProperty("location.a.z");
+		int bx = (int)getConfiguration().getProperty("location.b.x");
+		int bz = (int)getConfiguration().getProperty("location.b.z");
+		
+		int xmin = Math.min(ax, bx);
+		int zmin = Math.min(az, bz);
+		int xmax = Math.max(ax, bx);
+		int zmax = Math.max(az, bz);
+		
+		int spawnx = (int)((Math.random() * (xmax - xmin)) + xmin);
+		int spawnz = (int)((Math.random() * (zmax - zmin)) + zmin);
+		
+		World world = player.getWorld();
+		
+		int spawny = 127;
+		for(int y = 127; y>4; y--) {
+			 int blockId = world.getBlockAt(spawnx, y, spawnz).getTypeId();
+			 if(blockId != 0) {
+				 spawny = y+2;
+				 break;
+			 }
+		}
+		
+		Location loc = new Location(world, spawnx, spawny, spawnz);
+		getConfiguration().setProperty("location.hastp."+plname, true);	// Memory of previous players who first joined to not randomly tp them everytime they join.
+		getConfiguration().save();
+		Bukkit.getLogger().info("["+getName(this)+"] Player '" + plname + "' joined and got teleported to " + spawnx + ", " + spawny + ", " + spawnz + "... Magic!");
+		
+		return loc;
+	}
+	
+	// Get plugin name
+		private String getName(RandomTP rtp) {
+			return rtp.getDescription().getName();
+		}
+			
+		// Get plugion version
+		private String getVersion(RandomTP rtp) {
+			return rtp.getDescription().getVersion();
+		}
+			
+		// Config version
+		private int getConfigVersion() {
+			return 1000; // First version of Config version.
+		}
+
+}
